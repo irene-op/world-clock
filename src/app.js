7bb1582d2cbc6ae23f85af9fb8936274dfa58a3d
Irene Ortega <72819188+irene-op@users.noreply.github.com>
2023-08-24 17:10:50 +0200

    Display changes when selecting a city from drop-down menu. The time data is not working properly. Needs fixing

--- src/app.js
+++ src/app.js
@@ -3,19 +3,25 @@
 function updateTime(){
     // L.A.
     let losAngelesElement = document.querySelector("#los-angeles");
-    let losAngelesDateElement = losAngelesElement.querySelector(".date")
-    let losAngelesTimeElement = losAngelesElement.querySelector(".time")
+    if (losAngelesElement) {
+        let losAngelesDateElement = losAngelesElement.querySelector(".date")
+        let losAngelesTimeElement = losAngelesElement.querySelector(".time")
 
-    losAngelesDateElement.innerHTML = moment().tz("America/Los_Angeles").format("MMMM D, YYYY");
-    losAngelesTimeElement.innerHTML = moment().tz("America/Los_Angeles").format("hh:mm:ss [<small>]A[</small>]");
+        losAngelesDateElement.innerHTML = moment().tz("America/Los_Angeles").format("MMMM D, YYYY");
+        losAngelesTimeElement.innerHTML = moment().tz("America/Los_Angeles").format("hh:mm:ss [<small>]A[</small>]");
+    }
+    
 
     // Sydney
     let sydneyElement = document.querySelector("#sydney");
-    let sydneyDateElement = sydneyElement.querySelector(".date")
-    let sydneyTimeElement = sydneyElement.querySelector(".time")
+    if (sydneyElement) {
+        
+        let sydneyDateElement = sydneyElement.querySelector(".date")
+        let sydneyTimeElement = sydneyElement.querySelector(".time")
 
-    sydneyDateElement.innerHTML = moment().tz("Australia/Sydney").format("MMMM D, YYYY");
-    sydneyTimeElement.innerHTML = moment().tz("Australia/Sydney").format("hh:mm:ss [<small>]A[</small>]");
+        sydneyDateElement.innerHTML = moment().tz("Australia/Sydney").format("MMMM D, YYYY");
+        sydneyTimeElement.innerHTML = moment().tz("Australia/Sydney").format("hh:mm:ss [<small>]A[</small>]");
+    }
 }
 
 updateTime();
@@ -24,10 +30,21 @@ setInterval(updateTime, 1000);
 // Select and display new city from drop-down menu
 function displayCity(event) {
     let cityTimeZone = event.target.value;
-    let cityTime = moment().tz("cityTimeZone").format("hh:mm:ss");
-    let cityDate = moment().tz("cityTimeZone").format("MMMM D, YYYY");
+    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+    let cityTime = moment().tz("cityTimeZone");
+    let citiesElement = document.querySelector("#display-cities");
+    citiesElement.innerHTML = `
+    <div class="city">
+                <div>
+                    <h2>${cityName}</h2>
+                    <div class="date">${cityTime.format("MMMM D, YYYY")}</div>
+                </div>
+                <div class="time">${cityTime.format("hh:mm:ss")} <small>${cityTime.format("A")}</small></div>
+            </div>`;
+    
     console.log(cityTimeZone);
-    console.log(cityDate);
+    console.log(cityTime.format("hh:mm:ss A"));
+    //console.log(cityDate);
 }
 
 let selectCityElement = document.querySelector("#select-city")
